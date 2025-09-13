48bbab92dfa9a3dbf13311f4cd69acca4ae45975
sangwookpark <sangwookp9591@gmail.com>
2025-09-13 23:34:28 +0900

    feat : navigation type추가

---
+++ src/navigation/type.ts
@@ -0,0 +1,20 @@
+export type RootStackParamList = {
+  Splash: undefined;
+  Auth: undefined;
+  Main: undefined;
+  Onboarding: undefined;
+};
+
+export type AuthStackParamList = {
+  Login: undefined;
+  SignUp: undefined;
+  ForgotPassword: undefined;
+};
+
+export type MainTabParamList = {
+  Home: undefined;
+  Tracking: undefined;
+  Projects: undefined;
+  Analytics: undefined;
+  Profile: undefined;
+};
