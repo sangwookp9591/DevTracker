f282a99318c4d4241d7969bce5c8baa3b028db65
sangwookpark <sangwookp9591@gmail.com>
2025-09-13 23:36:15 +0900

    feat :AppNavigator 추가

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,10 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainNavigator from './MainNavigator';
 import { RootStackParamList } from './type';
+import OnboardingScreen from '../screens/auth/OnboardingScreen';
+import { useAuthStore } from '../store';
+import AuthNavigator from './AuthNavigator';
+import SplashScreen from '../screens/splash/SplashScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator: React.FC = () => {
+  const { isAuthenticated /*isFirstLaunch,checkAuthStatus*/ } = useAuthStore();
+
+  // useEffect(() => {
+  //   checkAuthStatus();
+  // }, [checkAuthStatus]);
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -16,10 +25,37 @@ const AppNavigator: React.FC = () => {
         }}
       >
         <Stack.Screen
-          name="Main"
-          component={MainNavigator}
+          name="Splash"
+          component={SplashScreen}
           options={{ animationTypeForReplace: 'push' }}
         />
+        {!isAuthenticated ? (
+          <>
+            {/* {isFirstLaunch && (
+              <Stack.Screen
+                name="Onboarding"
+                component={OnboardingScreen}
+                options={{ animationTypeForReplace: 'push' }}
+              />
+            )} */}
+            <Stack.Screen
+              name="Onboarding"
+              component={OnboardingScreen}
+              options={{ animationTypeForReplace: 'push' }}
+            />
+            <Stack.Screen
+              name="Auth"
+              component={AuthNavigator}
+              options={{ animationTypeForReplace: 'pop' }}
+            />
+          </>
+        ) : (
+          <Stack.Screen
+            name="Main"
+            component={MainNavigator}
+            options={{ animationTypeForReplace: 'push' }}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
